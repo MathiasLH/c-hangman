ee71120d33a0682c58e6559b2ba4e02633798002
MathiasLH <jupperify@gmail.com>
2019-05-16 22:08:43 +0200

    music is now playing throughout the game.

--- hangman/Form1.cs
+++ hangman/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,15 @@ namespace hangman
         public Form1()
         {
             InitializeComponent();
+                      
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("trying to play music");
+            SoundPlayer sound = new SoundPlayer(Properties.Resources.wii);
+            sound.Play();
         }
     }
 }
